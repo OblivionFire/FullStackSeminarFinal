bbade0ed9aa41abaa68e61f5649f9261d1f83bab
Jacob Patrick <fivestaroblivion@gmail.com>
2018-04-16 08:33:50 -0500

    Final of Full Stack Seminar

---
+++ DatabaseActivities/Models/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using DatabaseActivities.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace DatabaseActivities.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.AddFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Base> Base { get; set; }
+        public DbSet<PoundToKilo> PoundsToKilos { get; set; }
+        public DbSet<HorsetoBlock> HorsestoBlocks { get; set; }
+        public DbSet<MeterToFoot> MeterstoFeet { get; set; }
+        public DbSet<GramToOunce> GramToOunceBD { get; set; }
+    }
+}
